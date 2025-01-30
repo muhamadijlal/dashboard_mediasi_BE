eba6e6129995201e62572d8314043a814f69cf23
idarijul <m.haidarijl@gmail.com>
2025-01-30 09:11:59 +0700

    20250130/creat/add migrations

---
+++ database/migrations/2025_01_30_013937_create_tbl_integrator.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tbl_integrator', function (Blueprint $table) {
+            $table->id();
+            $table->string('ruas_id');
+            $table->string('ruas_nama');
+            $table->string('gerbang_id');
+            $table->string('gerbang_nama');
+            $table->string('host');
+            $table->integer('port');
+            $table->string('user');
+            $table->string('pass');
+            $table->string('database');
+            $table->string('database_schema')->nullable();
+            $table->enum('integrator', [1, 2, 3, 4]);
+            $table->boolean('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tbl_integrator');
+    }
+};
---
+++ database/seeders/RuasIntegratorSeeder.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RuasIntegratorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $tbl_ruas = [
+            [
+                'ruas_id' => '111',
+                'ruas_nama' => "JAGORAWI-DEV",
+                'gerbang_id' => '77',
+                'gerbang_nama' => "CIAWI 1-DEV",
+                'host' => '175.10.1.101',
+                'port' => 3306,
+                'user' => 'dbdev',
+                'pass' => 'jmto2024',
+                'database' => 'jago_lattol_77_source',
+                'integrator' => 2,
+            ],
+            [
+                'ruas_id' => '888',
+                'ruas_nama' => "DALKOT-DEV",
+                'gerbang_id' => '88',
+                'gerbang_nama' => "CILILITAN-DEV",
+                'host' => '175.10.1.101',
+                'port' => 3306,
+                'user' => 'dbdev',
+                'pass' => 'jmto2024',
+                'database' => 'miy_source',
+                'integrator' => 2,
+            ],
+            [
+                'ruas_id' => '999',
+                'ruas_nama' => "JANGER-DEV",
+                'gerbang_id' => '99',
+                'gerbang_nama' => "TANGERANG1-DEV",
+                'host' => '10.10.100.20',
+                'port' => 3306,
+                'user' => 'jmto',
+                'pass' => 'password',
+                'database' => 'dbsahrejmto',
+                'integrator' => 2,
+            ],
+        ];
+
+        $tbl_integrator = [
+            [
+                'ruas_id' => '111',
+                'ruas_nama' => "JAGORAWI-DEV",
+                'gerbang_id' => '77',
+                'gerbang_nama' => "CIAWI 1-DEV",
+                'host' => '175.10.1.101',
+                'port' => 3306,
+                'user' => 'dbdev',
+                'pass' => 'jmto2024',
+                'database' => 'jago_lattol_77_source',
+                'database_schema' => '',
+                'integrator' => 2,
+            ],
+            [
+                'ruas_id' => '888',
+                'ruas_nama' => "DALKOT-DEV",
+                'gerbang_id' => '88',
+                'gerbang_nama' => "CILILITAN-DEV",
+                'host' => '175.10.1.101',
+                'port' => 3306,
+                'user' => 'dbdev',
+                'pass' => 'jmto2024',
+                'database' => 'miy_source',
+                'database_schema' => '',
+                'integrator' => 2,
+            ],
+            [
+                'ruas_id' => '999',
+                'ruas_nama' => "JANGER-DEV",
+                'gerbang_id' => '99',
+                'gerbang_nama' => "TANGERANG1-DEV",
+                'host' => '10.10.100.20',
+                'port' => 3306,
+                'user' => 'jmto',
+                'pass' => 'password',
+                'database' => 'dbsahrejmto',
+                'database_schema' => 'karawaci1',
+                'integrator' => 2,
+            ],
+        ];
+
+        DB::connection("mysql")
+        ->table("tbl_integrator")
+        ->insert($tbl_integrator);
+
+        DB::connection("mysql")
+        ->table("tbl_ruas")
+        ->insert($tbl_ruas);
+    }
+}
