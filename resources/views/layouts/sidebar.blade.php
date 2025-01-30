d86593a5f73e3b213f7af4034284190d4085dbd8
idarijul <m.haidarijl@gmail.com>
2025-01-30 16:40:14 +0700

    20250130/fix/fix sidebar active menu

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -47,7 +47,7 @@
 
         <li>
             <details {{ request()->routeIs('data_compare.digital_receipt.*') ? "open" : ""}}>
-                <summary class="hover:bg-yellow-400 p-1 pr-2 flex items-center justify-between text-base font-medium rounded-lg">
+                <summary class="hover:bg-yellow-400 p-1 pr-2 flex items-center justify-between text-base font-medium rounded-lg {{ request()->routeIs('data_compare.digital_receipt.*') ? "bg-yellow-400" : "" }}">
                     <a href="#" class="flex items-center gap-2">
                         <div class="w-8 h-8 rounded-md bg-blue-950 flex justify-center items-center">
                         <i class="fa-solid fa-file-invoice text-yellow-400"></i>
