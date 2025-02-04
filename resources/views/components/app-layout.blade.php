34f04aa4ea761a348bd5ec54d2c9d9f9cc6e2240
idarijul <m.haidarijl@gmail.com>
2025-02-04 10:45:03 +0700

    20250204/create/add tab icon

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -5,9 +5,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>{{ str_replace("_"," ", config('app.name')) }} | {{ config('app.version') }}</title>
+  <link rel="icon" href="{{asset("assets/image/logojm.ico")}}">
+
 
   <!-- Fonts -->
-  <link rel="preconnect" href="https://fonts.bunny.net">
   <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
   <link href="https://cdn.datatables.net/2.2.1/css/dataTables.tailwindcss.css" rel="stylesheet" />
