ed254305c69b1a74589b15803dc13fdcdd5f6e99
idarijul <m.haidarijl@gmail.com>
2025-02-10 16:35:37 +0700

    20250210/fix/fix undefined props metoda_bayar_sah

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Validation\Rules\Password;
 use Illuminate\Validation\Rule;
 
 class ProfileController extends Controller
